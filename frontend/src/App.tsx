8a99337624ebb91a106dea5dbd318eaffd11431f
evhalp <evhalp@gmail.com>
2026-02-04 09:24:11 -0500

    Remove unnecessary semicolon in App.tsx import statement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import FileManager from './components/FileManager';
+import FileManager from './components/FileManager'
 
 function App() {
   // const [count, setCount] = useState(0)
