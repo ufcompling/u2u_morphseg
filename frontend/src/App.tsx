fcc66b1c49079fc845c108c5346cfc858e4c319c
ine <inepark22@gmail.com>
2026-02-05 15:15:27 -0500

    changed App.tsx to have analyzer not filemanager, and vite version changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import { FileManager } from './components/FileManager';
+// import { FileManager } from './components/FileManager';
+import { AnalyzerModule } from './features/analyzer/AnalyzerModule';
 
 export default function App() {
-  return <FileManager />;
+  // return <FileManager />;
+  return <AnalyzerModule />;
 }
