08e04b0bed7326b5d1c3e8af2cfe16e89a9a3852
evhalp <evhalp@gmail.com>
2026-02-13 20:49:53 -0500

    Remove commented-out code for micropip installation in initPyodide

--- src/services/pyodide/pyodideService.ts
+++ src/services/pyodide/pyodideService.ts
@@ -9,12 +9,6 @@ export const initPyodide = async () => {
 
   // Load micropip and install packages
   await pyodideInstance.loadPackage('micropip');
-  //const micropip = pyodideInstance.pyimport('micropip');
-
-  // Install python-crfsuite from whl and sklearn-crfsuite from PyPI
-  // const wheelUrl = window.location.origin + '/u2u_morphseg/wheels/python_crfsuite-0.9.12-cp312-cp312-pyodide_2024_0_wasm32.whl';
-  // await micropip.install(wheelUrl);
-  // await micropip.install('sklearn-crfsuite');
 
   await pyodideInstance.runPythonAsync(`
     import micropip
